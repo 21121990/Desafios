9fed5c19fa149fc09580694abddd7b58a809a585
DAYVSON SILVINO DA SILVA <62804086+21121990@users.noreply.github.com>
2021-05-08 16:17:59 -0300

    Desafio Inverter

--- desafios/Program.cs
+++ desafios/Program.cs
@@ -6,51 +6,63 @@ namespace desafios
     {
         static void Main(string[] args)
         {
-           // CrescimentoPopulacional();
-          //  Bazinga();
-          TempoEvento();
+            // CrescimentoPopulacional();
+            //  Bazinga();
+            // TempoEvento();
+            Inverter();
         }
-public static void TempoEvento(){
-     int diaInicio, diaTermino, horaMomentoInicio, minutoMomentoInicio, segundoMomentoInicio;
-        int horaMomentoTermino, minutoMomentoTermino, segundoMomentoTermino;
-        //continue escrevendo a solucao
+        public static void Inverter()
+        {
+            string n = Console.ReadLine();
+            //continue a solução
+            char[] arrayChar = n.ToCharArray();
+            Array.Reverse(arrayChar);
 
-        string[] dadosInicio = Console.ReadLine().Split();
-        diaInicio = Convert.ToInt32(dadosInicio[1]);
+            Console.WriteLine(arrayChar);
+            Console.ReadLine();
+        }
+        public static void TempoEvento()
+        {
+            int diaInicio, diaTermino, horaMomentoInicio, minutoMomentoInicio, segundoMomentoInicio;
+            int horaMomentoTermino, minutoMomentoTermino, segundoMomentoTermino;
+            //continue escrevendo a solucao
 
-        string[] dadosMomentoInicio = Console.ReadLine().Split(":");
-        horaMomentoInicio = Convert.ToInt32(dadosMomentoInicio[0]);
-        minutoMomentoInicio = Convert.ToInt32(dadosMomentoInicio[1]);
-        segundoMomentoInicio = Convert.ToInt32(dadosMomentoInicio[2]);
+            string[] dadosInicio = Console.ReadLine().Split();
+            diaInicio = Convert.ToInt32(dadosInicio[1]);
 
-
-        string[] dadosTermino = Console.ReadLine().Split();
-        diaTermino = Convert.ToInt32(dadosTermino[1]);
-
-        string[] dadosMomentoTermino = Console.ReadLine().Split(":");
-        horaMomentoTermino = Convert.ToInt32(dadosMomentoTermino[0]);
-        minutoMomentoTermino = Convert.ToInt32(dadosMomentoTermino[1]);
-        segundoMomentoTermino = Convert.ToInt32(dadosMomentoTermino[2]);
-
-
-        int transformaSegundosInicio = (segundoMomentoInicio + (diaInicio * 86400) + (horaMomentoInicio * 3600) + (minutoMomentoInicio * 60));
-        int transformaSegundosTermino = (segundoMomentoTermino + (diaTermino * 86400) + (horaMomentoTermino * 3600) + (minutoMomentoTermino * 60));
-        int somaTotalSegundos = (transformaSegundosTermino - transformaSegundosInicio);
-
-        int W = somaTotalSegundos / 86400;
-        int X = (somaTotalSegundos - (W * 86400)) / 3600;
-        int Y = (somaTotalSegundos - ((W * 86400) + (X * 3600))) / 60;
-        int Z = somaTotalSegundos - ((W * 86400) + (X * 3600) + (Y * 60));
+            string[] dadosMomentoInicio = Console.ReadLine().Split(":");
+            horaMomentoInicio = Convert.ToInt32(dadosMomentoInicio[0]);
+            minutoMomentoInicio = Convert.ToInt32(dadosMomentoInicio[1]);
+            segundoMomentoInicio = Convert.ToInt32(dadosMomentoInicio[2]);
 
 
-        Console.WriteLine("{0} dia(s)", W);
-        Console.WriteLine("{0} hora(s)", X);
-        Console.WriteLine("{0} minuto(s)", Y);
-        Console.WriteLine("{0} segundo(s)", Z);
-        
-Console.ReadLine();
+            string[] dadosTermino = Console.ReadLine().Split();
+            diaTermino = Convert.ToInt32(dadosTermino[1]);
 
-}
+            string[] dadosMomentoTermino = Console.ReadLine().Split(":");
+            horaMomentoTermino = Convert.ToInt32(dadosMomentoTermino[0]);
+            minutoMomentoTermino = Convert.ToInt32(dadosMomentoTermino[1]);
+            segundoMomentoTermino = Convert.ToInt32(dadosMomentoTermino[2]);
+
+
+            int transformaSegundosInicio = (segundoMomentoInicio + (diaInicio * 86400) + (horaMomentoInicio * 3600) + (minutoMomentoInicio * 60));
+            int transformaSegundosTermino = (segundoMomentoTermino + (diaTermino * 86400) + (horaMomentoTermino * 3600) + (minutoMomentoTermino * 60));
+            int somaTotalSegundos = (transformaSegundosTermino - transformaSegundosInicio);
+
+            int W = somaTotalSegundos / 86400;
+            int X = (somaTotalSegundos - (W * 86400)) / 3600;
+            int Y = (somaTotalSegundos - ((W * 86400) + (X * 3600))) / 60;
+            int Z = somaTotalSegundos - ((W * 86400) + (X * 3600) + (Y * 60));
+
+
+            Console.WriteLine("{0} dia(s)", W);
+            Console.WriteLine("{0} hora(s)", X);
+            Console.WriteLine("{0} minuto(s)", Y);
+            Console.WriteLine("{0} segundo(s)", Z);
+
+            Console.ReadLine();
+
+        }
         public static void CrescimentoPopulacional()
         {
             int t = Convert.ToInt32(Console.ReadLine());
@@ -96,25 +108,26 @@ Console.ReadLine();
             Console.ReadLine();
 
         }
-        public static void Bazinga(){
+        public static void Bazinga()
+        {
 
-              int qtdTeste = int.Parse(Console.ReadLine());
+            int qtdTeste = int.Parse(Console.ReadLine());
             string v1, v2;
-            for (int i = 1; i <=qtdTeste ; i++) //insira a variavel correta
+            for (int i = 1; i <= qtdTeste; i++) //insira a variavel correta
             {
                 string[] valores = Console.ReadLine().Split();
                 v1 = valores[0];
                 v2 = valores[1];
-               if ((v1 == "tesoura" && v2 == "papel") || (v1 == "papel" && v2 == "pedra") //complete a solução
-                || (v1 == "pedra" && v2 == "lagarto") || (v1 == "lagarto" && v2 == "Spock")
-                || (v1 == "Spock" && v2 == "tesoura") || (v1 == "tesoura" && v2 == "lagarto")
-                || (v1 == "lagarto" && v2 == "papel") || (v1 == "papel" && v2 == "Spock")
-                || (v1 == "Spock" && v2 == "pedra") || (v1 == "pedra" && v2 == "tesoura"))
-                Console.WriteLine("Caso #{0}: Bazinga!", i);
-            else if (v1 == v2)   //complete a solucao
-                Console.WriteLine("Caso #{0}: De novo!", i);
-            else
-                Console.WriteLine("Caso #{0}: Raj trapaceou!", i);
+                if ((v1 == "tesoura" && v2 == "papel") || (v1 == "papel" && v2 == "pedra") //complete a solução
+                 || (v1 == "pedra" && v2 == "lagarto") || (v1 == "lagarto" && v2 == "Spock")
+                 || (v1 == "Spock" && v2 == "tesoura") || (v1 == "tesoura" && v2 == "lagarto")
+                 || (v1 == "lagarto" && v2 == "papel") || (v1 == "papel" && v2 == "Spock")
+                 || (v1 == "Spock" && v2 == "pedra") || (v1 == "pedra" && v2 == "tesoura"))
+                    Console.WriteLine("Caso #{0}: Bazinga!", i);
+                else if (v1 == v2)   //complete a solucao
+                    Console.WriteLine("Caso #{0}: De novo!", i);
+                else
+                    Console.WriteLine("Caso #{0}: Raj trapaceou!", i);
             }
             Console.ReadLine();
         }
