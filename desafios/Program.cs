713fa5595589961303253e3450bfca27c4cefa99
DAYVSON SILVINO DA SILVA <62804086+21121990@users.noreply.github.com>
2021-07-23 18:43:12 -0300

    Update

--- desafios/Program.cs
+++ desafios/Program.cs
@@ -35,42 +35,76 @@ namespace desafios
             // preechimentovetor1();
             //trigoTabuleiro();
             // encaixe();
-            media3();
+            //media3();
+            TempoJogoMinutos();
+
+        }
+        public static void TempoJogoMinutos()
+        {
+            int Hi, Mi, Hf, Mf, MTI, MTF, H, M;
+            string[] s = Console.ReadLine().Split(' ');
+            Hi = int.Parse(s[0]);
+            Mi = int.Parse(s[1]);
+            Hf = int.Parse(s[2]);
+            Mf = int.Parse(s[3]);
+            MTI = Hi * 60 + Mi;
+            MTF = Hf * 60 + Mf;
+
+
+            if (MTI >= MTF)
+            {
+                Hf = Hf + 24;
+                MTI = Hi * 60 + Mi;
+                MTF = Hf * 60 + Mf;
+                MTF = MTF - MTI;
+                H = MTF / 60;
+                M = MTF % 60;
+                Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", H, M);
+            }
+            else
+            {
+                MTF = MTF - MTI;
+                H = MTF / 60;
+                M = MTF % 60;
+                Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", H, M);
+            }
+
+            Console.ReadLine();
 
         }
         public static void media3()
         {
-            float a, b, c, d, avg;
+            double a, b, c, d, N2, avg, NF;
             string[] values = Console.ReadLine().Split(' ');
-            a = float.Parse(values[0]);
-            b = float.Parse(values[1]);
-            c = float.Parse(values[2]);
-            d = float.Parse(values[3]);
-            avg = ((a * 2 + b * 3 + c * 4 + d * 1) / 10);            
-            Console.WriteLine("Media: " + Math.Round(avg,1).ToString("0.0"));
-            if (avg >= (7.0))
+            a = double.Parse(values[0]);
+            b = double.Parse(values[1]);
+            c = double.Parse(values[2]);
+            d = double.Parse(values[3]);
+            avg = ((a * 2) + (b * 3) + (c * 4) + (d * 1)) / 10;
+            Console.WriteLine("Media: " + Math.Round(avg, 1).ToString("0.0"));
+            if (avg >= 7.0)
             {
                 Console.WriteLine("Aluno aprovado.");
             }
-            if (avg < (5.0))
+            else if (avg < 5.0)
             {
                 Console.WriteLine("Aluno reprovado.");
             }
-            if (avg >= 5.0 &&  avg <= (6.9))
+            else if (avg >= 5.0 && avg <= 6.9)
             {
-                Console.WriteLine("Aluno em exame.");                
-                float e = float.Parse(Console.ReadLine());
-                Console.WriteLine("Nota do exame: " + Math.Round(e,1).ToString("0.0"));
-                avg = (e + avg) / 2;
-                if (avg >= (5.0))
+                Console.WriteLine("Aluno em exame.");
+                N2 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Nota do exame: " + Math.Round(N2, 1).ToString("0.0"));
+                NF = (N2 + avg) / 2;
+                if (NF >= (5.0))
                 {
                     Console.WriteLine("Aluno aprovado.");
                 }
-                if (avg <= (4.9))
+                else
                 {
                     Console.WriteLine("Aluno reprovado");
                 }
-                Console.WriteLine("Media final: " + Math.Round(avg,1).ToString("0.0"));
+                Console.WriteLine("Media final: " + Math.Round(NF, 1).ToString("0.0"));
             }
 
             Console.ReadKey();
